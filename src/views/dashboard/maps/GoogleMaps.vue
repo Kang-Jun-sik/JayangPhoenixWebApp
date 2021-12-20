d855a3fa50b073f92fc88882dc87c36e88b5256c
kang <kcu1436@naver.com>
2021-12-20 11:19:52 +0900

    - [경기장 정보 세팅]

--- src/views/dashboard/maps/GoogleMaps.vue
+++ src/views/dashboard/maps/GoogleMaps.vue
@@ -8,13 +8,13 @@
       <v-col cols="12">
         <base-material-card
             color="success"
-            title="Satellite Map"
+            title="실내코트(더샷체육관)"
             class="px-5 py-3"
         >
           <v-card-text class="px-0 pb-0">
             <v-sheet>
               <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26356315.701909266!2d-113.65984645545673!3d36.25591957613731!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited+States!5e0!3m2!1sen!2sus!4v1566158729223!5m2!1sen!2sus"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3160.1402355417713!2d127.18207171506492!3d37.62238897978748!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cb6f182927691%3A0x58dea88e35f761a!2z6rK96riw64-EIOuCqOyWkeyjvOyLnCDqsr3stpjroZw2OTHrsojquLggMTM!5e0!3m2!1sko!2skr!4v1639962734377!5m2!1sko!2skr"
                   width="100%"
                   height="450"
                   frameborder="0"
@@ -32,13 +32,13 @@
       >
         <base-material-card
             color="success"
-            title="Satellite Map"
+            title="야외코트1(뚝섬 장애인 농구장)"
             class="px-5 py-3"
         >
           <v-card-text class="px-0 pb-0">
             <v-sheet>
               <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.15831228695!2d-74.11976206978034!3d40.697663747508045!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew+York%2C+NY!5e0!3m2!1sen!2sus!4v1566158235149!5m2!1sen!2sus"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3186.1151757302046!2d127.07351592595764!3d37.52897275946978!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca4fafe3bde4d%3A0x6f04dfe2fc0914b1!2z7ISc7Jq47Yq567OE7IucIOq0keynhOq1rCDsnpDslpHrj5kgNzA0LTM!5e0!3m2!1sko!2skr!4v1639963718321!5m2!1sko!2skr"
                   width="100%"
                   height="450"
                   frameborder="0"
@@ -56,13 +56,13 @@
       >
         <base-material-card
             color="success"
-            title="Custom Skin & Settings Map"
+            title="야외코트2(뚝섬 테니스코트 농구장)"
             class="px-5 py-3"
         >
           <v-card-text class="px-0 pb-0">
             <v-sheet>
               <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.15831228695!2d-74.11976206978034!3d40.697663747508045!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew+York%2C+NY!5e0!3m2!1sen!2sus!4v1566158235149!5m2!1sen!2sus"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3164.153195654847!2d127.07319061506195!3d37.527885879805254!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca4fbb1053613%3A0x5ff1d535658f062e!2z7ISc7Jq47Yq567OE7IucIOq0keynhOq1rCDsnpDslpHrj5kgNDEwLTE!5e0!3m2!1sko!2skr!4v1639963949032!5m2!1sko!2skr"
                   width="100%"
                   height="450"
                   frameborder="0"
@@ -76,3 +76,9 @@
     </v-row>
   </v-container>
 </template>
+
+<style>
+.v-application .font-weight-light{
+  font-weight: 600 !important;
+}
+</style>
