d35f7dd0d88cec5a6cf9812a77f4cdbc25a169da
kang <kcu1436@naver.com>
2021-12-27 17:53:41 +0900

    - vdialog 추가 예정

--- src/components/member/MemberListBoard.vue
+++ src/components/member/MemberListBoard.vue
@@ -21,6 +21,9 @@
 <script>
 export default {
   name: "MemberListBoard",
+  mounted() {
+
+  },
   data() {
     return {
       singleSelect: false,
---
+++ src/dialog/vdialog01.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-dialog
+      v-model="dialog3"
+      max-width="300"
+  >
+    <v-card>
+      <v-card-title>
+        Are you sure?
+
+        <v-spacer/>
+
+        <v-icon
+            aria-label="Close"
+            @click="dialog3 = false"
+        >
+          mdi-close
+        </v-icon>
+      </v-card-title>
+
+      <v-card-text class="pb-6 pt-12 text-center">
+        <v-btn
+            class="mr-3"
+            text
+            @click="dialog3 = false"
+        >
+          Nevermind
+        </v-btn>
+
+        <v-btn
+            color="success"
+            text
+            @click="dialog3 = false"
+        >
+          Yes
+        </v-btn>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "vdialog01",
+  data: () => ({
+    dialog3: false
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
