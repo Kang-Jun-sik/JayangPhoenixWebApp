6d6f0650c36f87ce19f947501052073e32181a45
kang <kcu1436@naver.com>
2021-12-28 15:49:39 +0900

    - commit

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,22 @@
 module.exports = {
-  devServer: {
-    disableHostCheck: true,
-  },
-
-  transpileDependencies: ['vuetify'],
-
-  pluginOptions: {
-    i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: false,
+    outputDir: "../src/main/resources/static",
+    indexPath: "../static/index.html",
+    devServer: {
+        proxy: "http://localhost:8080"
     },
-  },
+    chainWebpack: config => {
+        const svgRule = config.module.rule("svg");
+        svgRule.uses.clear();
+        svgRule.use("vue-svg-loader").loader("vue-svg-loader");
+    },
+    transpileDependencies: ['vuetify'],
+
+    pluginOptions: {
+        i18n: {
+            locale: 'en',
+            fallbackLocale: 'en',
+            localeDir: 'locales',
+            enableInSFC: false,
+        },
+    },
 }
